1f861af37c775609622e0b9ed1bf4b9d2ee10f0d
Hiroo Hirose <delpiroo@gmail.com>
2023-07-24 09:08:30 +0900

    Initial commit

--- plot_graph.py
+++ plot_graph.py
@@ -70,4 +70,5 @@ def index():
     return render_template('index.html', graphJSON=graphJSON)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) if 'get_ipython' in globals() else app.run()
+
